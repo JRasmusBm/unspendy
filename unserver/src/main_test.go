31a93f5cd8a0eb967cd40df5fbfd3250692b8ce8
jrasmusbm <JRasmusBm@gmail.com>
2024-05-25 22:18:51 +0200

    refactor: inline parseJSON

--- unserver/src/main_test.go
+++ unserver/src/main_test.go
@@ -9,17 +9,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func parseJSON(r io.ReadCloser, v interface{}) error {
-	// Read the entire content into a byte slice
-	data, err := io.ReadAll(r)
-	if err != nil {
-		return err
-	}
-
-	// Unmarshal the JSON data into the provided interface
-	return json.Unmarshal(data, v)
-}
-
 type HealthResult struct {
 	Error bool `json:"error"`
 	Data  bool `json:"data"`
@@ -30,9 +19,10 @@ func TestHealthCheck(t *testing.T) {
 		req := httptest.NewRequest("GET", "/health", nil)
 		resp, _ := build_server().Test(req, -1)
 		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
 
 		var health_result HealthResult
-		err := parseJSON(resp.Body, &health_result)
+		json.Unmarshal(body, &health_result)
 
 		assert.Equal(t, nil, err)
 		assert.Equal(t, false, health_result.Error)
