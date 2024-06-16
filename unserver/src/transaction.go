553499491e9f89ba9ffefb648f4e14c22bc70259
jrasmusbm <JRasmusBm@gmail.com>
2024-06-16 17:02:14 +0200

    WIP

--- unserver/src/transaction.go
+++ unserver/src/transaction.go
@@ -1,9 +1,35 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"database/sql"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 type Transaction struct {
 	transaction_date string
+	// transaction_date: date = Field(validation_alias=AliasChoices("Date", "Datum"))
+	// description: str = Field(
+	//     validation_alias=AliasChoices("Name / Description", "Omschrijving")
+	// )
+	// account: str = Field(validation_alias=AliasChoices("Account", "Rekening"))
+	// counterparty: str = Field(
+	//     validation_alias=AliasChoices("Counterparty", "Rekening naam")
+	// )
+	// code: str = Field(validation_alias=AliasChoices("Code", "Tegenrekening"))
+	// is_debit: bool = Field(validation_alias=AliasChoices("Debit/credit", "Af Bij"))
+	// amount_in_cents: int = Field(
+	//     validation_alias=AliasChoices("Amount (EUR)", "Bedrag")
+	// )
+	// transaction_type: str = Field(
+	//     validation_alias=AliasChoices("Transaction type", "Mutatiesoort")
+	// )
+	// notifications: str = Field(
+	//     validation_alias=AliasChoices("Notifications", "Mededelingen")
+	// )
+	// resulting_balance_in_cents: int = Field(
+	//     validation_alias=AliasChoices("Resulting balance", "Saldo na mutatie")
+	// )
 }
 
 type TransactionSearchResultData struct {
@@ -15,7 +41,27 @@ type TransactionSearchResult struct {
 	Data  TransactionSearchResultData `json:"data"`
 }
 
-func register_transaction_routes(app *fiber.App) {
+func migrate_transactions(db *sql.DB) {
+	db.Exec(`
+	CREATE TABLE IF NOT EXISTS transactions (
+    id UUID 
+	);
+		`)
+}
+
+func save_transaction(db *sql.DB)  {
+	
+}
+
+
+
+func register_transaction_routes(app *fiber.App, db *sql.DB) {
+	app.Post("/transaction/upload", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"error": false,
+		})
+	})
+
 	app.Get("/transaction", func(c *fiber.Ctx) error {
 		return c.JSON(TransactionSearchResult{
 			Error: false,
--- unserver/src/upload_test.go
+++ unserver/src/upload_test.go
@@ -39,4 +39,26 @@ func TestUpload(t *testing.T) {
 		)
 	})
 
+	t.Run("With upload returns uploaded data", func(t *testing.T) {
+		db, err := sql.Open("sqlite3", "./temp_test.db")
+		defer db.Close()
+		server := build_server(db)
+		upload_resp, _ := server.Test(httptest.NewRequest("POST", "/transaction/upload", nil), -1)
+		assert.Equal(t, 200, upload_resp.StatusCode)
+		resp, _ := server.Test(httptest.NewRequest("GET", "/transaction", nil), -1)
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+
+		var transaction_search_result TransactionSearchResult
+		json.Unmarshal(body, &transaction_search_result)
+		fmt.Printf("%#v\n", transaction_search_result)
+
+		assert.Equal(t, nil, err)
+		assert.Equal(t,
+			NewTransactionSearchResult([]Transaction{
+				{transaction_date: "2024-05-13T09:14:22.000Z"},
+			}),
+			transaction_search_result,
+		)
+	})
 }
